60a85866a5041c59431082d5ebd3a902a1f95d83
OlgaMMC <40575788+OlgaMMC@users.noreply.github.com>
2018-06-26 15:22:59 -0400

    Update interaction.py

--- Interaction/interaction.py
+++ Interaction/interaction.py
@@ -1,5 +1,6 @@
 import igraph
 import random
+import networkx
 
 b=2
 c=1
@@ -11,14 +12,16 @@ def f(val):
 def interaction(G, set_nodes, b, c, f): 
   #realize all of the interactions between pairs of players belonging to the set
   for v in set_nodes:
-    N=neighborhood(v)
+    N=G[v]
     w=random.choice(N)
     #interact1(v,w)
     p=random.random()
+    #If the node w decides to help (with probability p_w(t)
     if p<=w.prob(t):
+      #update payoffs, fitness, probabilities, etc for next timestep
       w.y(t)+=b-c
-      v.y(t+1)=v.y()+b
-      v.Y(t+1)=v.Y(t)+v.y(t+1)
+      v.y(t+1)=b
+      v.Y(t)=v.Y(t-1)+v.y(t)
       v.prob(t+1)=f(v.Y(t))
   #return modified graph 
   return G 
