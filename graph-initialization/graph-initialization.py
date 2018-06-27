569ba5a6d83f8b8047f7d4e9bb117b0c6562d036
Laura Koemmpel <koemmpel@mit.edu>
2018-06-27 13:38:47 -0400

    Fixed bug with spacing

--- graph-initialization/graph-initialization.py
+++ graph-initialization/graph-initialization.py
@@ -12,26 +12,31 @@ OUTPUTS:
 import networkx as nx
 import matplotlib.pyplot as plt
 
-def generate_lattice(n, m, type, dim, periodic = False, with_positions = True, create_using = None):
-	if type == 'triangular':
-		lattice = nx.triangular_lattice_graph(10, 20, periodic=False, create_using=None)
-		nx.draw(lattice)
+def generate_lattice(n, m, type = 'triangular', dim = 2, periodic = False, with_positions = True, create_using = None):
+    try:
+        if type == 'triangular':
+            lattice = nx.triangular_lattice_graph(10, 20, periodic=False, create_using=None)
+            nx.draw(lattice)
+            plt.show()
+            return lattice
+    except ValueError:
+        print("The specified lattice type was invalid.")
+
+
+
+def generate_graph(n, m, type = 'hypercube', periodic=False, with_positions=True, create_using=None):
+	try:
+		if type == 'hypercube':
+			graph = nx.hypercube_graph(i)
+		else:
+			raise NameError
+
+		nx.draw(hg)
+
 		plt.show()
-	return lattice
 
+		return graph
+	except ValueError:
+		print("The specified graph type was invalid.")
 
-
-def generate_graph(n, m, type = hypercube, periodic=False, with_positions=True, create_using=None):
-	
-	if type == 'hypercube':
-		graph = nx.hypercube_graph(i)
-	else:
-		raise NameError
-
-	nx.draw(hg)
-
-	plt.show()
-
-	return graph
-
-
+generate_lattice(3,4)
