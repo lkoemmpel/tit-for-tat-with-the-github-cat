6c10c73c7e311df232721fc282e6b61b58403405
Laura Koemmpel <koemmpel@mit.edu>
2018-06-28 14:33:30 -0400

    Fixed node attribute code

--- Reproduction/reproduction.py
+++ Reproduction/reproduction.py
@@ -1,68 +1,70 @@
 import networkx as nx
 import random
+import matplotlib.pyplot as plt
 
-G = nx.triangular_lattice_graph(10,20)
-for n in self.G.nodes_iter:
-	fitness = random.uniform(0,1)
-	current_strat = random.randrange(0, 1, 1)
+# Initialization of graph to use for testing purposes
+G = nx.triangular_lattice_graph(4,3)
+for n in nx.nodes(G):
+    # Assign node attributes
+    # Initial strategy is C or D, chosen randomly
+    strategy_indicator = random.randrange(0, 1, 1)
+    if strategy_indicator == 1:
+        G.node[n]['strategy'] = 'Cooperate'
+    else:
+        G.node[n]['strategy'] = 'Defect'
+    # fitness attributes are chosen randomly
+    # print("The current strategy of node ", n, " is ", G.node[n]['strategy'] )
+    G.node[n]['fitness'] = random.uniform(0,1)
 
-
-
-
-
-	# associate to each node of the graph a tuple of its fitness and current state (C or D)
-	set_node_attributes(G, (fitness, strategy), n)
-
-print(G.adj)
-nx.draw(G,with_labels=True)
-plt.show()
 
 
 class strategy_update():
 
-	'''
-	make all of the reproduction process based on the fitness, weight and mutation parameters.
+    '''
+    make all of the reproduction process based on the fitness, weight and mutation parameters.
 
- 	'''
+    '''
 
-	# iterate through all nodes of the graph
-	# with probability p, consider allowing a node to reproduce
-	def __init__(self, G):
-		self.G = G
-
-
-	def birth_death(self):
-        fitness_dict = get_node_attributes(G, fitness)
+    # iterate through all nodes of the graph
+    # choose a random node proportinal to its fitness to reproduce
+    def __init__(self, G):
+        self.G = G
+    def birth_death(self):
+        fitness_dict = nx.get_node_attributes(G, 'fitness')
+        print("The fitness dictionary is ", fitness_dict)
         fitness_sum = sum(fitness_dict.values())
+        print("The fitness sum is ", fitness_sum)
 
-        cutoff = randrange(0, fitness_sum)
+        cutoff = random.uniform(0, fitness_sum)
+        print("The cutoff is ", cutoff)
 
-        for i in G.number_of_nodes():
-            if cutoff < fitness_dict[i]:
-                #This node will reproduce
+        # Iterate through all nodes of the graoh
+        # Note, i is a coordinate whose dimension depends on the dimension of the graph
+        for i in nx.nodes(G):
+            if G.node[i]['fitness'] < cutoff < G.node[i]['fitness']:
+                #This is the node that will reproduce
                 #so we now find a neighbor it can replace
-                reproduced_strategy = get_node_attributes(G, strategy, i)
+                reproduced_strategy = G.node[i]['strategy'] 
 
                 num_neighbors = len(all_neighbors(G,i))
-                death_index = random.uniform(0,num_neighbors)
+                j = randint(0, num_neighbors)
                 # set the strategy of the node selected to die to the strategy of the node selected to reproduce
-                set_node_attributes(G, reproduced_strategy, all_neighbors(G, i)[death_index])
+                print("Updating the strategy of node ", j, " from ", G.node[j]['strategy'], " to ", reproduced_strategy)
+                G.node[j]['strategy'] = reproduced_strategy
+                print("reproduction has finished for this round")
 
+        # Creates picture of graph 
+        nx.draw(G,with_labels=True)
+        plt.show()
+        return self.G
 
-	    nx.draw(G,with_labels=True)
-		plt.show()
-    	
+    def death_birth(self):
+        return self.G
 
+    def pairwise_comparison(self):
+        return self.G
 
-
-
-        return self.name
-
-    def death-birth(self):
-        return self.species
-
-    def pairwise-comparison(self):
-    	return G
-
+graph = strategy_update(G)
+new_graph = graph.birth_death()
+print(new_graph.adj)
     
-  	
