fb069fc8877339ce5d0c231aacdf83620a1e1111
“[Olga <[omedrano@mit.edu]>
2018-06-29 11:46:05 -0400

    working on timestep

--- Timestep-control/timestep-control-1.py
+++ Timestep-control/timestep-control-1.py
@@ -8,32 +8,35 @@ importlib.__import__('graph-initialization')
 importlib.__import__('interaction')
 importlib.__import__('reproduction')
 
+class game():
+	def __init__(self, G, game):
+		self.graph=G
+		self.game=game
+		self.t=0
+	def step(self):
+		G=self.graph
+		G=interaction(G)
+		G=reproduction(G)
+		return G
+	def set_nodes(self, game='BD'):
+		G=self.graph
+		if game=='BD':
+			return set(G.adj.keys())
 
-def choose_node_weighted_F(G, W):
-	nodes=G.nodes
-	x=random.choice(nodes, [W[i]/SF for i in range(n)])
-	return x
-	# for i in range(n):
-	# 	if W[i]<x<W[i+1]:
-	# 		chosen=i+1
-
-def timestep_control(G):
-	#determines when nodes should be deleted or reproduce according to probability distribution (e.g. poisson)
-	i=choose_node_weighted_F(G, S) #random vertex in G, use F_i
-	j=choose_node_weighted_F(G, C) #uniform random choice of vertex in G
-	#interaction and reproduction
-	G=interaction(G, i, j)
-	G=reproduction(G)
-	return G
+	def choose_node_weighted_F(G, W):
+		nodes=G.nodes
+		x=random.choice(nodes, [W[i]/SF for i in range(n)])
+		return x
 
 n=20
 m=30
 T=100000
-
 G=generate_lattice(n, m, 'triangular', 4)
 plt.show()
 
-#INITIALIZE GRAPH
+'''
+INITIALIZE GRAPHS
+'''
 
 #get the fitness values of the vertices in the graph
 SF=0
@@ -52,9 +55,22 @@ for t in range(T):
 nx.draw(hg)
 plt.show()
 
-# class Node(object):
-#   def __init__(self, prob, y, Y):
-#     self.prob=prob
-#     self.y=0
-#     self.Y=0
+'''
+GRAPH 1
+'''
+G=nx.triangular_lattice_graph(5,6)
+for v in nx.nodes(G):
+  #assign strategy with probability 1/2
+  choice=random.choice([0,1])
+  if choice==0:
+    G.node[v]['strategy']='Cooperate'
+  else:
+    G.node[v]['strategy']='Defect'
+  #itness takes some random value from 0 to 1
+  G.node[v]['fitness']=random.uniform(0,1)
+  #cooperative_state = probability of helping another node
+  G.node[v]['coop-state']=random.uniform(0,1)
 
+  #payoff from interactions
+  G.node[v]['turn-payoff']=0
+  G.node[v]['total-payoff']=0
